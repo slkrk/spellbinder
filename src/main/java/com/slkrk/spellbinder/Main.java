8eaf443c1767769e5b3531de8e8d1ca35ac9e4f6
Sebastian <sebastian.tatarczuch@gmail.com>
2019-12-02 22:11:47 +0100

    listener klawisza

--- src/main/java/com/slkrk/spellbinder/Main.java
+++ src/main/java/com/slkrk/spellbinder/Main.java
@@ -1,23 +1,45 @@
 package com.slkrk.spellbinder;
 
+import com.sun.javafx.util.Logging;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.util.logging.Logger;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
+
+
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("spellbinder.fxml"));
+
+
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
